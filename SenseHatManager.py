a27ca684d5aed482c73c92895cff49e0fd094670
sean <seanlimmac@Seans-MacBook-Pro.local>
2017-08-05 13:43:00 +0800

    [BUILD]

--- SenseHatManager.py
+++ SenseHatManager.py
@@ -24,7 +24,7 @@ def SENSE_disp_percent (percent):
     sense.set_pixels(disp_arr)
 
     # for debugging
-    return disp_arr
+    return 
 
 # Get temperature readings
 def SENSE_get_current_temp():
@@ -35,25 +35,3 @@ def SENSE_has_cumulative_movement(stats):
     movement_info =  list(sense.get_orientation_degrees().values())
     # Return movement and update vectors
     return True if sum(map(lambda x,y: abs(x - y),movement_info,stats)) > 20 else False, movement_info
-
-# SenseHat LED grid emulator purely for debugging purposes lol
-class SenseHAT_EMULATOR:
-
-    LED_map = {
-        (255,255,255) : '=',
-        (255,255,0) : '-',
-        (0,0,0) : '0'
-    }
-
-    def __init__(self,display_array):
-        print('SENSEHAT EMULATOR START')
-
-        self.update_grid(display_array)
-
-    def update_grid(self,display_array):
-        self.LED_grid = list(map(lambda x: self.LED_map[x],display_array))
-
-        print("\n\n")
-        for i in enumerate(self.LED_grid):
-            print('{:<6}'.format(i[1]) if i[0] % 8 != 0 else '\n\n{:<6}'.format(i[1]) , end= "")
-        print('\n\n')
